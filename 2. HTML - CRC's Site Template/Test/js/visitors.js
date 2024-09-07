638b8743f90b8b20816f75873ae08f6fd0d5d2db
Carlos Falci <ctfalci@gmail.com>
2024-09-07 12:01:40 +1200

    Visit Counter added  and working

--- 2. HTML - CRC's Site Template/Test/js/visitors.js
+++ 2. HTML - CRC's Site Template/Test/js/visitors.js
@@ -1,8 +1,8 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-        document.getElementById("visits").innerHTML = xhttp.responseText;
+async function atualizarContador() {
+    const response = await fetch('https://contador-visitas.ctfalci.workers.dev/');
+      const text = await response.text();
+      document.getElementById('contador').innerText = text;
     }
-};
-xhttp.open("GET", "https://uhn4ff5i12.execute-api.us-east-1.amazonaws.com/Prod/resume", true);
-xhttp.send();
+    
+    // Chame a função ao carregar a página
+    window.onload = atualizarContador;
