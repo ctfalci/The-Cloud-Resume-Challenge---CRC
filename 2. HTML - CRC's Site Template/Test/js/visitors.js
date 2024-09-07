1ccd5eb2043f5f2d63dd051620bd483d36132dac
Carlos Falci <ctfalci@gmail.com>
2024-09-07 12:43:55 +1200

    Deleted Counter

--- 2. HTML - CRC's Site Template/Test/js/visitors.js
+++
@@ -1,8 +0,0 @@
-async function atualizarContador() {
-    const response = await fetch('https://contador-visitas.ctfalci.workers.dev/');
-      const text = await response.text();
-      document.getElementById('contador').innerText = text;
-    }
-    
-    // Chame a função ao carregar a página
-    window.onload = atualizarContador;
